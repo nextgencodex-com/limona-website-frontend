112a198e6dc909c362702d00fbb0443efbf1e3a6
Vishwa Udhantha <vishwaudantha@gmail.com>
2025-11-26 20:57:00 +0530

    Frontend
    Done

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,9 +1,11 @@
 import LatestArrivals from '@/components/Limona-Home/Latest-Arrivals/page';
+import WhatWeProvide from '@/components/Limona-Home/WhatWeProvide/page';
 
 export default function Home() {
   return (
     <main>
       <LatestArrivals />
+      <WhatWeProvide />
     </main>
   )
 }
