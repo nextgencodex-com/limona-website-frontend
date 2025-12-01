0c3f1f92b7a7bdd6787f028bdaf9ed99386644ec
AvishkaNavindu <navinduavishka2002dfuze@gmail.com>
2025-12-01 14:37:10 +0530

    edited
    edited

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,13 +1,12 @@
 import HeroSection from '@/components/Limona-Home/Hero-Section/hero-section';
 import LatestArrivals from '@/components/Limona-Home/Latest-Arrivals/page';
-import PhotoWall from '@/components/Limona-Home/Photo-Wall/PhotoWall';
+
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <LatestArrivals />
-      <PhotoWall />
     </main>
   )
 }
