3aebee905ffaada544192fddb446b24c8f766da7
AvishkaNavindu <navinduavishka2002dfuze@gmail.com>
2025-12-01 12:20:55 +0530

    hero section
    hero section updated

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,9 +1,13 @@
+import HeroSection from '@/components/Limona-Home/Hero-Section/hero-section';
 import LatestArrivals from '@/components/Limona-Home/Latest-Arrivals/page';
+import PhotoWall from '@/components/Limona-Home/Photo-Wall/PhotoWall';
 
 export default function Home() {
   return (
     <main>
+      <HeroSection />
       <LatestArrivals />
+      <PhotoWall />
     </main>
   )
 }
