61830dfada0c6cd0a9d60a52c03a8f97584650ee
Vishwa Udhantha <vishwaudantha@gmail.com>
2025-12-10 23:20:54 +0530

    Frontend
    Done

---
+++ src/app/Printed-T-Shirt/page.tsx
@@ -0,0 +1,11 @@
+import LimonaPrintedTShirt from '@/components/Limona-Printed-T-Shirt/page';
+import Footer from '@/components/Limona-Footer/page';
+
+export default function PrintedTShirtPage() {
+  return (
+    <div>
+      <LimonaPrintedTShirt />
+      <Footer />
+    </div>
+  );
+}
