1d9cf78abc1f99318992d401f91e9ced5ecfec39
Gangulr <167494975+Gangulr@users.noreply.github.com>
2025-12-11 08:46:46 +0530

    navigation issues fixed

--- src/app/Customize-Your-Own/page.tsx
+++ src/app/Customize-Your-Own/page.tsx
@@ -1,9 +1,10 @@
 import LimonaCustomizeYourOwn from '@/components/Limona-Customize-Your-Own/page';
 import Footer from '@/components/Limona-Footer/page';
-
+import Header from '@/components/Limona-Header/page';
 export default function CustomizeYourOwnPage() {
   return (
     <div>
+      <Header />
       <LimonaCustomizeYourOwn />
       <Footer />
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import HeroHome from '@/app/Home/page';
+import Header from '@/components/Limona-Header/page';
+import Footer from '@/components/Limona-Footer/page';
 
 export default function HomePage() {
   return (
     <main>
+      
       <HeroHome />
+      
     </main>
   );
 }
