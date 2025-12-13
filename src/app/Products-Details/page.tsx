03cbf318ddd06f7cb6b085b78f83b8d659933306
Gangulr <167494975+Gangulr@users.noreply.github.com>
2025-12-13 09:26:17 +0530

    fix vercel issue

--- src/app/Products-Details/page.tsx
+++ src/app/Products-Details/page.tsx
@@ -1,5 +1,10 @@
-import ProductDetails from '@/components/Limona-Products-Details/page';
+import { Suspense } from "react";
+import ProductDetails from "@/components/Limona-Products-Details/page";
 
 export default function ProductDetailsPage() {
-  return <ProductDetails />;
+  return (
+    <Suspense fallback={<div>Loading product...</div>}>
+      <ProductDetails />
+    </Suspense>
+  );
 }
